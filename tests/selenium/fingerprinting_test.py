f2fae911c5f89c94b2648ff65516ae34aa5ab9e7
Alexei <alexeiatyahoodotcom@gmail.com>
2019-08-14 18:54:41 -0400

    Fix fp. test by switching to options instead of bg
    Avoids problems inherent in initializing another instance of Privacy
    Badger.

--- tests/selenium/fingerprinting_test.py
+++ tests/selenium/fingerprinting_test.py
@@ -5,36 +5,38 @@ import unittest
 
 import pbtest
 
-from functools import partial
-
-from pbtest import retry_until
-from window_utils import switch_to_window_with_url
-
 
 class FingerprintingTest(pbtest.PBSeleniumTest):
     """Tests to make sure fingerprinting detection works as expected."""
 
     def detected_fingerprinting(self, domain):
-        return self.js("""let tracker_origin = window.getBaseDomain("{}");
-return (
-  Object.keys(badger.tabData).some(tab_id => {{
-    let fpData = badger.tabData[tab_id].fpData;
-    return fpData &&
-      fpData.hasOwnProperty(tracker_origin) &&
-      fpData[tracker_origin].canvas &&
-      fpData[tracker_origin].canvas.fingerprinting === true;
-  }})
-);""".format(domain))
+        return self.js((
+            "let bg = chrome.extension.getBackgroundPage(),"
+            "  tracker_origin = window.getBaseDomain(arguments[0]);"
+            ""
+            "return ("
+            "  Object.keys(bg.badger.tabData).some(tab_id => {"
+            "    let fpData = bg.badger.tabData[tab_id].fpData;"
+            "    return fpData &&"
+            "      fpData.hasOwnProperty(tracker_origin) &&"
+            "      fpData[tracker_origin].canvas &&"
+            "      fpData[tracker_origin].canvas.fingerprinting === true;"
+            "  })"
+            ");"
+        ), domain);
 
     def detected_tracking(self, domain, page_url):
-        return self.js("""let tracker_origin = window.getBaseDomain("{}"),
-  site_origin = window.getBaseDomain((new URI("{}")).host),
-  map = badger.storage.snitch_map.getItemClones();
-
-return (
-  map.hasOwnProperty(tracker_origin) &&
-    map[tracker_origin].indexOf(site_origin) != -1
-);""".format(domain, page_url))
+        return self.js((
+            "let bg = chrome.extension.getBackgroundPage(),"
+            "  tracker_origin = window.getBaseDomain(arguments[0]),"
+            "  site_origin = window.getBaseDomain((new URI(arguments[1])).host),"
+            "  map = bg.badger.storage.snitch_map.getItemClones();"
+            ""
+            "return ("
+            "  map.hasOwnProperty(tracker_origin) &&"
+            "  map[tracker_origin].indexOf(site_origin) != -1"
+            ");"
+        ), domain, page_url)
 
     # TODO can fail because our content script runs too late: https://crbug.com/478183
     @pbtest.repeat_if_failed(3)
@@ -42,28 +44,27 @@ return (
         PAGE_URL = "https://www.eff.org/files/badger_test_fixtures/fingerprinting.html"
         FINGERPRINTING_DOMAIN = "cdn.jsdelivr.net"
 
-        # open Badger's background page
-        self.load_url(self.bg_url)
-
-        # need to keep Badger's background page open for tabData to persist
-        # so, open and switch to a new window
-        self.open_window()
-
         # visit the page
         self.load_url(PAGE_URL)
 
-        # switch back to Badger's background page
-        switch_to_window_with_url(self.driver, self.bg_url)
+        # since we use tabData below,
+        # and tabData gets cleaned up on navigation,
+        # open a new window
+        self.open_window()
+
+        # open the options page for querying
+        self.load_url(self.options_url)
 
         # check that we detected the fingerprinting domain as a tracker
         self.assertTrue(
-            retry_until(partial(self.detected_tracking, FINGERPRINTING_DOMAIN, PAGE_URL)),
-            "Canvas fingerprinting resource was detected as a tracker.")
+            self.detected_tracking(FINGERPRINTING_DOMAIN, PAGE_URL),
+            "Canvas fingerprinting domain was detected as a tracker."
+        )
 
         # check that we detected canvas fingerprinting
         self.assertTrue(
             self.detected_fingerprinting(FINGERPRINTING_DOMAIN),
-            "Canvas fingerprinting resources was detected as a fingerprinter."
+            "Canvas fingerprinting domain was detected as a fingerprinter."
         )
 
 
